2ff1bf119df1f6148a0fe62f1256806b9bed1ce0
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2024-09-18 16:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+let strong = document.querySelectorAll('strong')
+
 function highlight() {
     //Write your code here
 
-
+if (strong) {
+ strong.forEach(item =>{
+	 item.style.color = 'green';
+ });
+}
 }
 
 
 function return_normal() {
     //Write your code here
-
+if(strong){
+	strong.forEach(item =>{
+		item.style.color = 'initial'
+	});
+}
     
 }
